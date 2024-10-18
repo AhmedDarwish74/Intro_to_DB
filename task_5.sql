68b024cf4e0a9ecd7bc3905e424d321ec7bd021f
AhmedDarwish74 <ahmed.darwish774@gmail.com>
2024-10-18 06:28:46 +0300

    task5

---
+++ task_5.sql
@@ -0,0 +1,4 @@
+-- Script to insert a single row in the customers table
+
+INSERT INTO CUSTOMERS (CUSTOMER_ID, CUSTOMER_NAME, EMAIL, ADDRESS)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
