dee103767de32801aae64ea4a0beb9b74b0e77ab
AhmedDarwish74 <ahmed.darwish774@gmail.com>
2024-10-19 01:49:39 +0300

    edit

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,15 @@
 -- Specify the database to use
 USE alx_book_store;
 
--- Query to get the full description of the 'books' table
-SHOW CREATE TABLE Books;
+-- Get the description of the 'books' table
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM 
+    information_schema.COLUMNS
+WHERE 
+    TABLE_NAME = 'Books'
+    AND TABLE_SCHEMA = 'alx_book_store';
