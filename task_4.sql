8e97a4cc265f4a84c80d4def19668c745475212c
AhmedDarwish74 <ahmed.darwish774@gmail.com>
2024-10-19 01:48:24 +0300

    edit

--- task_4.sql
+++ task_4.sql
@@ -2,14 +2,4 @@
 USE alx_book_store;
 
 -- Query to get the full description of the 'books' table
-SELECT 
-    COLUMN_NAME AS 'Column Name',
-    COLUMN_TYPE AS 'Column Type',
-    IS_NULLABLE AS 'Is Nullable',
-    COLUMN_DEFAULT AS 'Default Value',
-    EXTRA AS 'Extra Info'
-FROM 
-    information_schema.COLUMNS
-WHERE 
-    TABLE_SCHEMA = 'alx_book_store' AND 
-    TABLE_NAME = 'books';
+SHOW CREATE TABLE Books;
